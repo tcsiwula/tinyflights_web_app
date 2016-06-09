5e7c6b423e501bf58a34c375ca6feef806c1ed75
brobusf21 <brandonrobinson21@me.com>
2016-06-08 18:14:10 -0700

    Minor adjustments. Disconnected from Heroku only for testing purposes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /* Load dependencies */
-var express = require('./config/express');
-var open = require('open');
-var app = express();
+var express = require('./config/express'),
+	open = require('open'),
+	app = express();
+
+// Heroku logic
+// app.listen(process.env.PORT || 5000, function()
+// {
+//     console.log("TinyFlights.com server listening on port %d [http://localhost:5000/]? in %s mode", this.address().port, app.settings.env);
+//     open('http://127.0.0.1:5000');
+// });
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
 
 /* Expose this application */
 module.exports = app;
-
-// Heroku logic
-app.listen(process.env.PORT || 5000, function()
-{
-    console.log("TinyFlights.com server listening on port %d [http://localhost:5000/]? in %s mode", this.address().port, app.settings.env);
-    open('http://127.0.0.1:5000');
-});
