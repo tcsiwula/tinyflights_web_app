08aa1a7886fbe0d3518c446c8f3bc0e2b2cda5f4
Tim Siwula <tcsiwula@usfca.edu>
2016-08-03 22:58:33 -0700

    updated favicon ;)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Use the 'NODE_ENV' variable to run on localhost or Heroku
 // To run in development mode through console/terminal:
-//     $ NODE_ENV=development node index.js
+// $ NODE_ENV=development node index.js
+// i think heroku uses 5000 as default, testing locally with heroku so changed
+// process.env.NODE_ENV = "development";
+
 process.env.NODE_ENV = "development";
 
 // Invoke 'strict' JavaScript mode
@@ -10,7 +13,9 @@ process.env.NODE_ENV = "development";
 var mongoose = require('./config/mongoose'),
 	express = require('./config/express'),
 	passport = require('./config/passport'),
+	favicon = require('serve-favicon'),			// for serving image/favicon
 	open = require('open'); // Not sure what this does...let me know! -- hahaha muahahaha #pacheco
+							// open automatically launches your web browser to view the app when the server starts i.e. open('http://127.0.0.1:5000');
 
 // Create a new Mongoose connection instance
 var db = mongoose();
@@ -18,18 +23,24 @@ var db = mongoose();
 // Create a new Express application instance
 var app = express(db);
 
+// load the happy plane logo
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
+
 // Configure the Passport middleware
 var passport = passport();
 
 // Set the 'NODE_ENV' variable
 if (process.env.NODE_ENV === 'development') {
 
-	app.listen(process.env.PORT || 5000);	// i think heroku uses 5000 as default, testing locally with heroku so changed
+	app.listen(process.env.PORT || 5000);
 	console.log('\n  \n  \n');
-	console.log("TinyFlights.com server listening on port %d http://localhost:3000 in %s mode", process.env.PORT, app.settings.env);
+	console.log("TinyFlights.com server listening on port http://localhost:%d in %s mode", process.env.NODE_ENV, app.settings.env);
 	open('http://127.0.0.1:5000');
 
  }
+
+
+
  // buggy for now, fix this logic later.
  //else if (process.env.NODE_ENV === 'production') {
 //
@@ -38,6 +49,8 @@ if (process.env.NODE_ENV === 'development') {
 //     	open('http://127.0.0.1:5000');
 // 	});
 //}
+
+
 
 /* Expose this application */
 module.exports = app;
