3304a35431210f3566800586baf74f8f2a2cd057
Tim Siwula <tcsiwula@usfca.edu>
2016-08-13 03:53:29 -0700

    test commit

--- config/express.js
+++ config/express.js
@@ -8,11 +8,17 @@ var config = require('./config'),
 	session = require('express-session'),
 	MongoStore = require('connect-mongo')(session),
 	flash = require('connect-flash'),
-	passport = require('passport');
+	passport = require('passport'),
+	favicon = require('serve-favicon'),			// for serving image/favicon
+	open = require('open'); // Not sure what this does...let me know! -- hahaha muahahaha #pacheco
+							// open automatically launches your web browser to view the app when the server starts i.e. open('http://127.0.0.1:5000');
 
 
 module.exports = function(db) {
 	var app = express();
+
+	// load the happy plane logo
+	app.use(favicon(__dirname + '/../public/img/favicon.ico'));
 
 	// Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
 	if (process.env.NODE_ENV === 'development') {
@@ -57,7 +63,7 @@ module.exports = function(db) {
 	require('../app/routes/users.server.routes.js')(app);
 	
 	/* Configure static file serving */
-	app.use(express.static('./public'));
+	app.use(express.static(__dirname +'/public'));
 
 	return app;
 };
